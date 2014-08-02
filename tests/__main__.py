028f25b8db2b113266aa56cb47b5134410d7255e
bdoms <ravenize@gmail.com>
2014-08-02 15:58:49 -0500

    Rely on app engine being in the PYTHONPATH for testing.

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,13 +3,16 @@ import os
 import sys
 import unittest
 
+try:
+    import dev_appserver
+except ImportError, e:
+    raise ImportError, "App Engine must be in PYTHONPATH."
+    sys.exit()
+
 os.environ["SERVER_SOFTWARE"] = "DevelopmentTesting"
 
 test_path = sys.argv[0]
-sdk_path = sys.argv[1]
 
-sys.path.insert(0, sdk_path)
-import dev_appserver
 dev_appserver.fix_sys_path()
 
 # fix_sys_path removes the current working directory, so we add it back in
@@ -17,7 +20,6 @@ sys.path.append('.')
 
 # needed to be able to import the third party libraries
 from config.constants import LIB_PATH
-sys.path.append(LIB_PATH)
 sys.path.append(os.path.join(LIB_PATH, 'webtest'))
 
 suite = unittest.loader.TestLoader().discover(test_path)
