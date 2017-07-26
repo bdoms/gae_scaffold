993bb4512f2118519de9b5d98716910275724c3d
bdoms <b@bdoms.com>
2017-07-26 09:58:29 -0500

    Added migrations and data reset actions to dev.
    Also now displaying namespace on dev.
    And updated namespace manager to match new GAE behavior.

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,5 @@
 import os
 
-from google.appengine.api import namespace_manager
 from google.appengine.ext import vendor
 
 from config.constants import LIB_PATH
@@ -14,8 +13,11 @@ vendor.add(os.path.join(LIB_PATH, 'gcs', 'python', 'src'))
 # uncomment this to make each version use its own namespace (which segregates the datastore)
 # called only if the current namespace is not set
 # def namespace_manager_default_namespace_for_request():
-#    version = os.environ.get('CURRENT_VERSION_ID', '').split(".")[0]
-#    if version == "None":
-#        return ""
-#    else:
-#        return version
+#     # versions for things we name are like "name.1234567890"
+#     # default versions are in the format YYYYMMDDtHHMMSS
+#     version = os.environ.get('CURRENT_VERSION_ID', '1').split('.')[0]
+#     if version[0].isdigit() or version == 'None':
+#         # the latter None case happens on local development
+#         return ''
+#     else:
+#         return version
--- controllers/dev.py
+++ controllers/dev.py
@@ -1,11 +1,16 @@
-from google.appengine.api import users, memcache
-from base import FormController
+import logging
 
+from google.appengine.api import users, memcache
+from google.appengine.api.namespace_manager import namespace_manager
+
+from base import FormController
+import helpers
 import model
 
 from gae_validators import validateEmail
 
 LOGOUT_URL = users.create_logout_url("/")
+NAMESPACE = namespace_manager.get_namespace()
 
 
 class DevController(FormController):
@@ -15,7 +20,7 @@ class DevController(FormController):
 
     def get(self):
 
-        self.renderTemplate('dev.html', logout_url=LOGOUT_URL)
+        self.renderTemplate('dev.html', namespace=NAMESPACE, logout_url=LOGOUT_URL)
 
     def post(self):
 
@@ -38,5 +43,35 @@ class DevController(FormController):
         elif self.request.get("memcache"):
             # clear memcache
             memcache.flush_all()
+            self.flash('info', 'Cleared Memcache')
+
+        elif self.request.get('migrate'):
+            logging.info('Beginning migration.')
+            modified = []
+
+            # do migration work
+            #q = model.User.query()
+            #for item in q:
+            #    modified.append(item)
+
+            if modified:
+                model.ndb.put_multi(modified)
+
+            logging.info('Migration finished. Modified ' + str(len(modified)) + ' items.')
+            self.flash('success', 'Migrations Complete')
+
+        elif self.request.get('reset') and helpers.debug():
+            # delete all entities for all classes
+            model_classes = [model.Auth, model.User]
+            for model_class in model_classes:
+                entities = model_class.query().fetch(keys_only=True)
+                if len(entities) > 0:
+                    model.ndb.delete_multi(entities)
+
+            # add any fixtures needed for development here
+
+            # auto signout since the IDs and keys have all changed
+            self.session.clear()
+            self.flash('info', 'Data Reset')
 
         self.redisplay()
